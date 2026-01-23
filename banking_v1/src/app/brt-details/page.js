2d2fad29f781c6e690835cf3045df0b348a7b21c
vamshik-skysecure <vamshik.shetty@skysecure.ai>
2026-01-23 10:03:16 +0530

    Enabled Hosting Process

--- banking_v1/src/app/brt-details/page.js
+++ banking_v1/src/app/brt-details/page.js
@@ -1,11 +1,17 @@
+"use client";
+
+export const dynamic = "force-dynamic";
+
+import { Suspense } from "react";
 import BRTDetailsPage from "@/components/BRTDetailsPage";
 import Providers from "@/components/Providers";
 
 export default function BRTDetailsRoutePage() {
   return (
     <Providers>
-      <BRTDetailsPage />
+      <Suspense fallback={<div>Loading...</div>}>
+        <BRTDetailsPage />
+      </Suspense>
     </Providers>
   );
 }
-
